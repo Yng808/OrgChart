167050531ba159341d825f22ebc67f6647c27498
Chris <butugruv@gmail.com>
2024-02-10 22:12:51 -1000

    update orgChart to intake from List<People>

---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace OrgChart.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string ParentId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; } // If you want to include last names separately
+        public string Position { get; set; }
+        public string Image { get; set; }
+    }
+}
--- wwwroot/orgChart.js
+++ wwwroot/orgChart.js
@@ -38,3 +38,48 @@
             .render();
     });
 }
+
+function initChartWithJson(jsonData) {
+    let chart;
+    const data = JSON.parse(jsonData).map(d => ({
+        id: d.Id,
+        parentId: d.ParentId,
+        name: `${d.Name} ${d.LastName}`, // Combining Name and LastName
+        position: d.Position,
+        image: d.Image
+    }));
+
+    chart = new d3.OrgChart()
+
+        .nodeHeight((d) => 85 + 25)
+        .nodeWidth((d) => 220 + 2)
+        .childrenMargin((d) => 50)
+        .compactMarginBetween((d) => 35)
+        .compactMarginPair((d) => 30)
+        .neighbourMargin((a, b) => 20)
+        .nodeContent(function (d, i, arr, state) {
+            const color = '#FFFFFF';
+            const imageDiffVert = 25 + 2;
+            return `
+                <div style='width:${d.width
+                }px;height:${d.height}px;padding-top:${imageDiffVert - 2}px;padding-left:1px;padding-right:1px'>
+                        <div style="font-family: 'Inter', sans-serif;background-color:${color};  margin-left:-1px;width:${d.width - 2}px;height:${d.height - imageDiffVert}px;border-radius:10px;border: 1px solid #E4E2E9">
+                            <div style="display:flex;justify-content:flex-end;margin-top:5px;margin-right:8px">#${d.data.id
+                }</div>
+                            <div style="background-color:${color};margin-top:${-imageDiffVert - 20}px;margin-left:${15}px;border-radius:100px;width:50px;height:50px;" ></div>
+                            <div style="margin-top:${-imageDiffVert - 20
+                }px;">   <img src=" ${d.data.image}" style="margin-left:${20}px;border-radius:100px;width:40px;height:40px;" /></div>
+                            <div style="font-size:15px;color:#08011E;margin-left:20px;margin-top:10px">  ${d.data.name
+                } </div>
+                            <div style="color:#716E7B;margin-left:20px;margin-top:3px;font-size:10px;"> ${d.data.position
+                } </div>
+
+                        </div>
+                    </div>
+                            `;
+        })
+        // configuration remains the same...
+        .container('.chart-container')
+        .data(data) // Use the parsed data here
+        .render();
+}
